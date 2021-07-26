5fb2ec11ee789ecffe69454d6e51af4ee8c02d39
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-07-25 22:02:41 -0500

    Añandiendo los conexión mediante la cadena de conexión de postgreSql

--- WebServerCovid/Startup.cs
+++ WebServerCovid/Startup.cs
@@ -27,6 +27,9 @@ namespace WebServerCovid
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connect_postgre = new Config_postgres(Configuration.GetConnectionString("Config_postgres"));
+            services.AddSingleton(connect_postgre);
+            services.AddScoped<ICovid, Provin_reposito>();
             services.AddControllers();
         }
 
