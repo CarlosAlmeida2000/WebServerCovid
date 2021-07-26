0b84638dd743bb71116679171db889d5035fa7bc
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-07-25 21:56:04 -0500

    Implementación del modelo

--- Api_covid.models/Provincia.cs
+++ Api_covid.models/Provincia.cs
@@ -4,6 +4,15 @@ namespace Api_covid.models
 {
     public class Provincia
     {
-      
+        public int idprovincia { get; set; }
+        public string provincia { get; set; }
+        public string capital { get; set; }
+        public double superficie { get; set; }
+        public double hombres { get; set; }
+        public double mujeres { get; set; }
+        public double problacion_total { get; set; }
+        public double contagiados { get; set; }
+        public double recuperados { get; set; }
+        public double fallecidos { get; set; }
     }
 }
