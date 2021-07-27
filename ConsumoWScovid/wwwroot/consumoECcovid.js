74c65f9ce461229f6d750a3c40207f4580d37937
jhonp <john.plazarte2017@uteq.edu.ec>
2021-07-26 19:22:30 -0500

    charts añadidos, c,f y r

--- ConsumoWScovid/wwwroot/consumoECcovid.js
+++ ConsumoWScovid/wwwroot/consumoECcovid.js
@@ -1,6 +1,8 @@
 ﻿// Traer las provincias
 var labelsProv = [];
-var dataProv = [];
+var dataContg = [];
+var dataFallec = [];
+var dataRecup = [];
 
 var listaprovincia = document.getElementById("lista-provincias");
 $.ajax({
@@ -11,15 +13,90 @@ $.ajax({
     success: function (data) {
         data.forEach((element) => {
             labelsProv.push(element.provincia);
-            dataProv.push(element.problacion_total);
+            dataContg.push(element.contagiados);
+            dataFallec.push(element.fallecidos);
+            dataRecup.push(element.recuperados);
 
             //mostrar provincias
-            $("#lista-provincias").append("<li>" + element.provincia + "</li>");
+            $("#lista-provincias").append('<li class="item">' + element.provincia + '</li>');
         });
-        mostrarProvincias();
-        //dibujarChartProvincias();
+        dibujarChartContagiados();
+        dibujarChartFallecidos();
+        dibujarChartRecuperados();
     },
     error: function (data) {
         alert("Ocurrió un error al intentar traer las provincias");
     },
 });
+function dibujarChartContagiados() {
+    var contextoProv = document
+        .getElementById("canvas-contagiados")
+        .getContext("2d");
+    var chartContag = new Chart(contextoProv, {
+        type: "line",
+        data: {
+            labels: labelsProv,
+            datasets: [
+                {
+                    label: "Contagiados",
+                    data: dataContg,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+function dibujarChartFallecidos() {
+    var contextoProv = document
+        .getElementById("canvas-fallecidos")
+        .getContext("2d");
+    var chartFallec = new Chart(contextoProv, {
+        type: "line",
+        data: {
+            labels: labelsProv,
+            datasets: [
+                {
+                    label: "Fallecidos",
+                    data: dataFallec,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+function dibujarChartRecuperados() {
+    var contextoProv = document
+        .getElementById("canvas-recuperados")
+        .getContext("2d");
+    var chartRecup = new Chart(contextoProv, {
+        type: "line",
+        data: {
+            labels: labelsProv,
+            datasets: [
+                {
+                    label: "Recuperados",
+                    data: dataRecup,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
