826a7c36175e9bbf55dc2f43e8b5891e3e3b375b
DarlynYami05 <dwverling@gmail.com>
2021-07-26 23:50:09 -0500

    FrontEnd terminado

---
+++ ConsumoWScovid/wwwroot/mapaEcCovid.js
@@ -0,0 +1,21 @@
+﻿$.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    url: "https://localhost:5001/api/contagios_ecuador/",
+    dataType: "json",
+    success: function (data) {
+        var i = 0;
+        data.forEach( (element) => {
+            
+            var path_tag = document.getElementsByTagName('path');
+            path_tag[i].getElementsByTagName('title')[0].textContent = element.provincia 
+                + "\nContagiados: " + element.contagiados + "\nFallecidos: " 
+                + element.fallecidos + "\nRecuperados: " 
+                + element.recuperados;
+            i++;
+        } );
+    },
+    error: function (data) {
+        
+    },
+});
